bd2a65286d6a9461aac8c2005a59521116aca8eb
mtkrtk <mtkrtk@gmail.com>
2015-09-17 20:23:03 +0900

    Add read methods

--- hogehogefoofoobarbar/Robot_APIs/PAL3G4200D.cpp
+++ hogehogefoofoobarbar/Robot_APIs/PAL3G4200D.cpp
@@ -1,6 +1,35 @@
 #include "PAL3G4200D.h"
+#include <usb_serial.h>
 
-PAL3G4200D::PAL3G4200D(I2CDevice::Pin pin) : device(pin, 0)
+PAL3G4200D::PAL3G4200D(SPIDevice::Pin pin) : device(pin, SPIDevice::Baud::DIV_8, SPIDevice::Mode::Mode2)
 {
+    if (device.read(whoAmI) != 211) {
+        SerialUSB.println("error reading whoami");
+    }
     
+    /* disable sleep mode */
+    uint8_t ctrl1 = device.read(CTRL_REG1);
+    ctrl1 &= ~(1 << PD);
+    device.write(CTRL_REG1, ctrl1);
+}
+
+int16_t PAL3G4200D::readX()
+{
+    Data data;
+    device.read(OUT_X_L, data.buf, 2);
+    return data.value;
+}
+
+int16_t PAL3G4200D::readY()
+{
+    Data data;
+    device.read(OUT_Y_L, data.buf, 2);
+    return data.value;
+}
+
+int16_t PAL3G4200D::readZ()
+{
+    Data data;
+    device.read(OUT_Z_L, data.buf, 2);
+    return data.value;
 }
--- hogehogefoofoobarbar/Robot_APIs/PAL3G4200D.h
+++ hogehogefoofoobarbar/Robot_APIs/PAL3G4200D.h
@@ -1,13 +1,26 @@
 #ifndef __PAL3G4200D__
 #define __PAL3G4200D__
 
-#include "../Core_APIs/I2CDevice.h"
+#include "../Core_APIs/SPIDevice.h"
 
 class PAL3G4200D {
-    I2CDevice device;
+    static constexpr uint8_t whoAmI = 15;
+    static constexpr uint8_t CTRL_REG1 = 32;
+    static constexpr uint8_t PD = 3;
+    static constexpr uint8_t OUT_X_L = 40;
+    static constexpr uint8_t OUT_Y_L = 42;
+    static constexpr uint8_t OUT_Z_L = 44;
+    SPIDevice device;
+    union Data {
+        uint8_t buf[2];
+        uint16_t value;
+    };
     
 public:
-    PAL3G4200D(I2CDevice::Pin pin);
+    PAL3G4200D(SPIDevice::Pin pin);
+    int16_t readX();
+    int16_t readY();
+    int16_t readZ();
 };
 
 #endif
