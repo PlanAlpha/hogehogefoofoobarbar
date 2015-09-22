7e6c2f63883799548f9d7557f8d4f1dddd8827be
mtkrtk <mtkrtk@gmail.com>
2015-09-22 12:11:16 +0900

    Add motor class

---
+++ hogehogefoofoobarbar/Robot_APIs/GCMotor.cpp
@@ -0,0 +1,35 @@
+#include "GCMotor.h"
+#include <limits>
+
+GCMotor::GCMotor(uint8_t _pin1, uint8_t _pin2) : pin1(_pin1), pin2(_pin2)
+{
+    free();
+}
+
+void GCMotor::forward(int32_t power)
+{
+    if (power >= 0) {
+        pin2 = 0;
+        pin1 = power;
+    } else {
+        pin1 = 0;
+        pin2 = -power;
+    }
+}
+
+void GCMotor::forward(float power)
+{
+    if (power >= 1) {
+        forward(std::numeric_limits<int32_t>::max());
+    } else if (power <= -1) {
+        forward(std::numeric_limits<int32_t>::min());
+    } else {
+        forward(static_cast<int32_t>(power * std::numeric_limits<int32_t>::max()));
+    }
+}
+
+void GCMotor::free()
+{
+    pin1 = 0;
+    pin2 = 0;
+}
---
+++ hogehogefoofoobarbar/Robot_APIs/GCMotor.h
@@ -0,0 +1,17 @@
+#ifndef __GCMotor__
+#define __GCMotor__
+
+#include "../Core_APIs/PwmOut.h"
+
+class GCMotor {
+    PwmOut pin1;
+    PwmOut pin2;
+    
+public:
+    GCMotor(uint8_t pin1, uint8_t pin2);
+    void forward(int32_t power);
+    void forward(float power);
+    void free();
+};
+
+#endif
