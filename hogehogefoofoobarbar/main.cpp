0109c40baa8e9cf3eabf2dd318eb719de9b76af5
jaga0228_mac <jaga0228@gmail.com>
2015-10-13 15:11:05 +0900

    LineTrace

--- hogehogefoofoobarbar/main.cpp
+++ hogehogefoofoobarbar/main.cpp
@@ -4,25 +4,37 @@
 #include "DigitalIn.h"
 #include "Core_APIs/InterruptIn.h"
 #include <limits>
+
 using namespace PlanAlpha;
 
 int main(int __attribute__((unused)) argc, const char __attribute__((unused)) * argv[])
 {
-	delay(2000);
 	speaker.play(400, std::numeric_limits<uint16_t>::max()/2, 1000);
-    
-//    pinMode(8, PWM);
-//    pinMode(9, PWM);
-//    pwmWrite(8, 0);
-//    pwmWrite(9, std::numeric_limits<uint16_t>::max() / 2);
-//	rightMotor.forward(0.2);
-	
+	delay(2000);
 	SerialUSB.begin();
+	
+
+	//Linetrace with 2sensors.
+	while (1) {
+		if (forwardLineSensors.read() & PAThreeLineSensors::Left) {
+			leftMotor.forward(-0.5);
+			rightMotor.forward(0.5);
+		}else if (forwardLineSensors.read() & PAThreeLineSensors::Right){
+			leftMotor.forward(0.5);
+			rightMotor.forward(-0.5);
+		}else {
+			rightMotor.forward(0.5);
+			rightMotor.forward(0.5);
+		}
+		delay(10);
+	}
+	
+	
+	//view LineSensor's RawValues.
+	
 	DigitalIn touch(19);
 	DigitalIn touch2(18);
-	
 	while (1) {
-//		SerialUSB.print(forwardLineSensors.read() & PAThreeLineSensors::Left);
 		SerialUSB.print(forwardLeftLineSensor.readRawValue());
 		SerialUSB.print("\t");
 		SerialUSB.print(forwardCenterLineSensor.readRawValue()/2);
